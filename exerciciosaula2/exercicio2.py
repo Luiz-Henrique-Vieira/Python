66d0e928a07c5d669cc710e04edee86f5d5d7606
Luiz Henrique <103291286+Luiz-Henrique-Vieira@users.noreply.github.com>
2023-03-22 23:18:19 -0300

    Delete exerciciosaula2 directory

--- exerciciosaula2/exercicio2.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system('cls')
-
-satual= float (input('Digite seu salario atual: '))
-snovo = satual + (satual * (5/100))
-
-
-print(f'Seu salario novo é: {snovo:.2f}')
-print(f"Sua comissão foi de: {satual * 5/100: .2f}")
