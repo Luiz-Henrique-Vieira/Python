6eaaf17383576e95fa21882ab8c8029c768297ce
Luiz Henrique <103291286+Luiz-Henrique-Vieira@users.noreply.github.com>
2023-04-07 16:20:44 -0300

    Add files via upload

---
+++ exerciciosaula6/exercicio3.py
@@ -0,0 +1,11 @@
+import os
+os.system ('cls')
+
+num = int(input('Digite um numero inteiro: '))
+
+if num >=1:
+    print('Seu número é positivo')
+elif num == 0:
+    print('Seu numero é nulo')
+else:
+    print('Seu numero é negativo.')
