c2b8c681d1a7e8cd3273d38c3602fad24b6b1477
Luiz Henrique <103291286+Luiz-Henrique-Vieira@users.noreply.github.com>
2023-04-18 22:46:50 -0300

    Aula 4 com if and else

--- aula4/imparoupar.py
+++ aula4/imparoupar.py
@@ -8,10 +8,8 @@ num = int(input('Digite um número: '))
 if num%2 == 0:
     print(f'Seu numero {num} é par')
 else:
-=======
-import os
-os.system('cls')
 
+    
 num = int(input('Digite um número: '))
 
 
